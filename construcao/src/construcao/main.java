6cf73f37200b056c8fd616708b5ca0e906beca2f
Danilo Moreira <xliudanilox@gmail.com>
2018-02-23 22:22:17 -0300

    Primeiro Commit

--- construcao/src/construcao/main.java
+++ construcao/src/construcao/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package construcao;
 
 /**
  *
@@ -15,6 +15,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
     }
     
 }
